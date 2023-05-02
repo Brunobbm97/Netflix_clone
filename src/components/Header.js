b0d78d1a0224f239f1ae9f4ffa4019a3f1bddbf7
Bruno <brunobbmachado97@outlook.com>
2023-05-02 17:33:29 -0300

    Templates_1

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './Header.css';
+
+export default () => {
+    return(
+        <header className="black">
+            <div className="header--logo">
+                <a href="/">
+                    <img src="/assets/logo_netflix.png" alt="Netflix"/>
+                </a>
+            </div>
+            <div className="header--user">
+                <a href="/">
+                <img src="/assets/avatar.png" alt="Usuário"/>
+                </a>
+            </div>
+        </header>
+    )
+}
